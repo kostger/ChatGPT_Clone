5fb02cfe0409c47c0ea86390d68029f79bab9541
GEROGIANNIS KONSTANTINOS <kostger2@gmail.com>
2023-11-06 19:44:33 +0200

    Innit Commit
    Date 11/6/23

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ChatGPT_Clone
  A simple javascript project, cloning chatgpt with an unnoficial chatgpt api request.
+
+This is the api used.
+[API](https://rapidapi.com/haxednet/api/chatgpt-api8/)
+
+<h1> Screenshots </h1>
+
+![Alt text](./Screenshot_1.png "Optional Title")
+
+![Alt text](./Screenshot_2.png "Optional Title")
---
+++ index.js
@@ -0,0 +1,100 @@
+const chatLog = document.getElementById('chat-log'),
+userInput = document.getElementById('user-input'),
+sendButton = document.getElementById('send-button'),
+buttonIcon = document.getElementById('button-icon'),
+info = document.querySelector('.info');
+
+
+sendButton.addEventListener('click',sendMessage);
+userInput.addEventListener('keydown',(e) =>{
+    if(e.key === 'Enter'){
+        sendMessage();
+    }
+});
+
+function sendMessage(){
+    const message = userInput.value.trim();
+    if(message === ''){
+        return;
+    }else if(message ==='developer'){
+        userInput.value = '';
+        appendMessage('user',message);
+        setTimeout(() => {
+           appendMessage('bot','Coded by Konstantinos Gerogiannis');
+           buttonIcon.classList.add('fa-solid','fa-paper-plane');
+           buttonIcon.classList.remove('fas','fa-spinner','fa-pulse');
+        }, 2000);
+        return;
+    }
+
+    appendMessage('user',message);
+    userInput.value = '';
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'ce5210d53cmsh07e4c373eb39efdp176081jsn02f453c4b824',
+            'X-RapidAPI-Host': 'chatgpt-api8.p.rapidapi.com'
+        },
+        body: JSON.stringify([
+            {
+              content: message, 
+              role: 'user'
+            }
+        ])
+    };
+
+    fetch('https://chatgpt-api8.p.rapidapi.com/',options).then((response) =>
+        response.json()).then((response) => {
+            appendMessage('bot',response.text);
+
+            buttonIcon.classList.add('fa-solid',
+           'fa-paper-plane');
+           buttonIcon.classList.remove('fas','fa-spinner',
+           'fa-pulse');
+        }).catch((err) => { 
+            if(err.name === "TypeError"){
+                appendMessage('bot','Error: Cjecl Your Api Key!');
+                buttonIcon.classList.add('fa-solid',
+                'fa-paper-plane');
+                buttonIcon.classList.remove('fas','fa-spinner',
+                'fa-pulse');
+            }
+        });
+}
+
+function appendMessage(sender,message){
+    info.computedStyleMap.display = "none";
+    buttonIcon.classList.remove('fa-solid',
+    'fa-paper-plane');
+    buttonIcon.classList.add('fas','fa-spinner',
+    'fa-pulse');
+
+    const messageElement = document.createElement('div');
+    const iconElement = document.createElement('div');
+    const chatElement = document.createElement('div');
+    const icon = document.createElement('i');
+
+    chatElement.classList.add("chat-box");
+    iconElement.classList.add("icon");
+    messageElement.classList.add(sender);
+    messageElement.innerText = message;
+
+    //add icons depending on who send message
+    if(sender === 'user'){
+        icon.classList.add('fa-regular', 'fa-user');
+        iconElement.setAttribute('id','user-icon');
+    }
+    else{
+        icon.classList.add('fa-solid','fa-robot');
+        iconElement.setAttribute('id','bot-icon');
+    }
+
+    iconElement.appendChild(icon);
+    chatElement.appendChild(iconElement);
+    chatElement.appendChild(messageElement);
+    chatLog.appendChild(chatElement);
+    chatLog.scrollTo = chatLog.scrollHeight;
+    
+}
